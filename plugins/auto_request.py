c5254928f99979c32616b7d5d28c850504f2e7f2
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2024-04-26 10:25:18 +0530

    Update auto_request.py

--- plugins/auto_request.py
+++ plugins/auto_request.py
@@ -1,15 +1,24 @@
 import pyrogram
 from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 @Client.on_chat_join_request()
-async def request_handler(client: user, update):
+async def request_handler(client, update):
     user_id = update.from_user.id
+    user_m = update.from_user.mention
     chat_title = update.chat.title
     chat_id = update.chat.id
 
     await client.approve_chat_join_request(chat_id=chat_id, user_id=user_id)
-
-    await client.send_message(
+    photo_path = "https://telegra.ph/file/4ee563b2e8a1efc87e582.jpg"  
+    caption = f"Hello {user_m} ✨️\n\nYour Request to Join {chat_title} has been Approved.\n\nSend /start to know more.\nJoin US 👇👇"
+    buttons = [
+        [InlineKeyboardButton("requesting group", url="https://t.me/+N-d6LxO8-VozOTc9")],
+        [InlineKeyboardButton("latest movies", url="https://t.me/+ASrQmyP1AGIwOTU9")]
+    ]
+    await client.send_photo(
         chat_id=user_id,
-        text=f"Hello {user_id}! Welcome to {chat_title}"
+        photo=photo_path,
+        caption=caption,
+        reply_markup=InlineKeyboardMarkup(buttons)
     )
